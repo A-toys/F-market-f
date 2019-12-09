215decf89ea8fca1ee8547d30938f7729595ea04
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-09 20:36:06 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:stop'
-    invoke! 'unicorn:start'
+    invoke 'unicorn:start'
     # invoke 'unicorn:restart'
   end
 
