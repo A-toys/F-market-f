b012d2dbb04d550b1420fbb6beaf0fac080bd7ee
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-09 17:22:02 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items dependent: :destroy
-  has_many :comments dependent: :destroy
-  has_many :orders dependent: :destroy
-  has_one :card dependent: :destroy
-  has_one :address dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_one :card, dependent: :destroy
+  has_one :address, dependent: :destroy
 end
