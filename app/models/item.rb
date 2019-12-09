7ab76866014881d1320f2c7f50fe9d4754e0fb1a
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2019-12-09 18:25:13 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  has_many :comments dependent: :destroy
-  has_many :images dependent: :destroy
+  has_many :comments ,dependent: :destroy
+  has_many :images ,dependent: :destroy
   belongs_to :user
   has_one :order
   belongs_to :category
