5a48eb63be43ddd0e1e353eaed44f84f13e5c8fe
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-12-10 10:32:35 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,12 +15,14 @@ class User < ApplicationRecord
    user = User.find_by(email: auth.info.email)
 
    unless user
-     user = User.create(nickname: auth.info.name,
-                        provider: auth.provider,
-                        uid:      auth.uid,
-                        token:    auth.credentials.token,
-                        password: Devise.friendly_token[0, 20],
-                        meta:     auth.to_yaml)
+     user = User.create(
+      nickname: auth.info.name,
+      provider: auth.provider,
+      uid:      auth.uid,
+      token:    auth.credentials.token,
+      password: Devise.friendly_token[0, 20],
+      meta:     auth.to_yaml
+    )
      end
      user
    end
