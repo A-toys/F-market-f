05c18fd585ac973b8b5e4b5ee373133debab914a
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2019-12-11 20:43:33 +0900

    商品出品機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   get '/mypage', to: 'items#mypage'
   get '/registration', to: 'items#registration'
   get '/login', to: 'items#login'
-  resources :items, only: [:new]
+  resources :items, only: [:new,:create]
 end
