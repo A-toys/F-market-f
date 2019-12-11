d8938ea8b28d3b51ab8cf086e3e91427a9081243
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2019-12-11 20:43:33 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,23 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
+    @item.images.build
+    @item.build_category
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:text,:condition,:fee_burden,:service,:area,:handing_time,:price,:trading_status,:service,:category_id, 
+    images_attributes:[:image_url]).merge(seller_user_id:1)
   end
 end
