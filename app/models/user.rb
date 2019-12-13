fb4ee7236eb54ae390930dff0ad7f7ef6e28a9b3
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-12-13 14:01:33 +0900

    メールアドレス、住所の登録及びログインログアウト機能実装
    #what
    ユーザー登録機能実装
    
    #why
    ユーザー情報を保持し、
    出品機能などを使う場合にユーザーの情報入力の手間を省かせるため。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :omniauthable, omniauth_providers: %i(google)
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(google)
+
+
+  # validation
+  validates :nickname, length: { in: 1..15, message: 'は1〜15文字で記入してください'}, presence: true,on: :validates_step1
+  validates :email, length: { maximum: 50, too_long: 'は50文字以内で記入してください'}, presence: true, format: { with: /\A\S+@\S+\.\S+\z/, message: "は「***@***.***」で登録してください"},on: :validates_step1
+  validates_uniqueness_of :email, message: 'は既に登録されています' ,on: :validates_step1
+  validates :password, length: { in: 7..15, message: '6〜15文字で記入してください'}, presence: true,on: :validates_step1
+  validates :last_name, presence: true,on: :validates_step1
+  validates :first_name, presence: true,on: :validates_step1
+  validates :last_name_kana, presence: true,on: :validates_step1
+  validates :first_name_kana, presence: true,on: :validates_step1
+  validates :birthday, presence: true,on: :validates_step1
+  validates :phone_number, length: { in: 10..11, message: 'は10,11文字で記入してください'}, presence: true,on: :validates_step2
+  validates_uniqueness_of :phone_number, message: 'は既に登録されています',on: :validates_step2
 
   has_many :items, dependent: :destroy
   has_many :comments, dependent: :destroy
