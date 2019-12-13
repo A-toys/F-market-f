a6418cd028b4e51cabf12ea63f76a093c7ffa156
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-13 18:48:59 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -78,14 +78,14 @@ ActiveRecord::Schema.define(version: 2019_12_11_092256) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "phone_number", null: false
+    t.string "nickname"
+    t.string "email"
+    t.string "encrypted_password", default: ""
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "phone_number"
     t.text "icon"
     t.text "introduce"
     t.string "reset_password_token"
@@ -96,8 +96,6 @@ ActiveRecord::Schema.define(version: 2019_12_11_092256) do
     t.string "uid"
     t.string "token"
     t.string "meta"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
