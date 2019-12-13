875092602c61167b5134852d4a9a3fbfd2e5494d
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-13 19:33:35 +0900

    Update items_controller.rb
    debugg

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name,:text,:condition,:fee_burden,:service,:area,:handing_time,:price,:trading_status,:service,:category_id, 
     images_attributes:[:image_url]).merge(seller_user_id:1)
   end
-
- 
 end
