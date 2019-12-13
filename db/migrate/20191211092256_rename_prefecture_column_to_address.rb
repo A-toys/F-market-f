0329b2973cfc1237be2e01f443fb8522d1ba6975
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-13 21:09:00 +0900

    Update 20191211092256_rename_prefecture_column_to_address.rb

--- db/migrate/20191211092256_rename_prefecture_column_to_address.rb
+++ db/migrate/20191211092256_rename_prefecture_column_to_address.rb
@@ -1,5 +1,5 @@
 class RenamePrefectureColumnToAddress < ActiveRecord::Migration[5.2]
   def change
-    rename_column :Addresses, :prefecture, :prefecture_id
+    rename_column :addresses, :prefecture, :prefecture_id
   end
 end
