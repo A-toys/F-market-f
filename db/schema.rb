c53db7ed460072884e91f98f3e15ccd5e1a35ea8
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-13 18:32:55 +0900

    revert

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_084225) do
+ActiveRecord::Schema.define(version: 2019_12_09_062019) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_084225) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+<<<<<<< HEAD
     t.string "users"
     t.string "provider"
     t.string "uid"
@@ -103,6 +104,10 @@ ActiveRecord::Schema.define(version: 2019_12_12_084225) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+=======
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+>>>>>>> parent of b6da2ba... migrate
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
