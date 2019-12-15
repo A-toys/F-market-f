13333c4163c197a61e3de990c2cbe9808f8affee
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-15 14:29:47 +0900

    payjp

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,12 +1,12 @@
-$(function(){
-  $('.slider__box').slick({
-    autoplay: true,
-    speed: 800,
-    autoplaySpeed: 4000,
-    dots:true,
-    // arrows: true,
-    prevArrow: '<span class="prev-arrow">&lt;</span>',
-    nextArrow: '<span class="next-arrow">&gt;</span>',
-    adaptiveHeight: true,
-  })
-});
+// $(function(){
+//   $('.slider__box').slick({
+//     autoplay: true,
+//     speed: 800,
+//     autoplaySpeed: 4000,
+//     dots:true,
+//     // arrows: true,
+//     prevArrow: '<span class="prev-arrow">&lt;</span>',
+//     nextArrow: '<span class="next-arrow">&gt;</span>',
+//     adaptiveHeight: true,
+//   })
+// });
