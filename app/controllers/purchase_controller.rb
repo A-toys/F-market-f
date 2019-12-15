86266a14af519731cbb65d88902aa203b0ab5d49
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-16 00:11:18 +0900

    view adjustment

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -2,6 +2,7 @@ class PurchaseController < ApplicationController
   require 'payjp'
 
   def index
+    @item = Item.find(params[:item_id])
     card = Card.where(user_id: current_user.id).first
     #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
     if card.blank?
@@ -17,16 +18,20 @@ class PurchaseController < ApplicationController
   end
 
   def pay
+    @item = Item.find(params[:item_id])
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = "sk_test_0e2395683608b1f458779eb8"
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
   )
   redirect_to action: 'done' #完了画面に移動
   end
 
+  def done
+    @item = Item.find(params[:item_id])
+  end
   private
 
   def set_card
