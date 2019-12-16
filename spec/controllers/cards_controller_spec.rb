a93d193a3875b41af6e709c926479185b1634f2e
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-16 14:16:42 +0900

    Update cards_controller_spec.rb

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe CardsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'GET #show' do
+    it "renders the :show template" do
+      get :show, params: { id: 1 }
+      expect(response).to render_template :show
+    end
+  end
+
 end
