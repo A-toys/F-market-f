ce7a2e7348976450e3d65ed38f261ec37a95e90e
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-16 14:01:34 +0900

    create testcode

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe CardsController, type: :controller do
     @user = FactoryBot.create(:user)
   end
 
+  describe 'GET #new' do
+    it "renders the :new template" do
+      get :new, params: { id: 1 }
+      expect(response).to render_template :new
+    end
+  end
 end
