7cb073c3b446a3e11759660e3bf0adf9dea94743
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-18 18:26:10 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,5 +17,5 @@ CarrierWave.configure do |config|
     config.storage :file
     config.enable_processing = false if Rails.env.test?
   end
-  config.asset_host = 'https://yuki-baguette.s3-ap-northeast-1.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yuki-baguette'
 end
