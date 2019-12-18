65ba3a5ffc817fbcf7d670ec2b061bde888becf8
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2019-12-18 12:12:50 +0900

    up

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 2019_12_16_055953) do
     t.integer "price", null: false
     t.integer "trading_status", null: false
     t.bigint "seller_user_id", null: false
-    t.integer "category", null: false
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -98,7 +97,7 @@ ActiveRecord::Schema.define(version: 2019_12_16_055953) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
@@ -110,6 +109,11 @@ ActiveRecord::Schema.define(version: 2019_12_16_055953) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "users"
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
+    t.string "meta"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "birthday"
