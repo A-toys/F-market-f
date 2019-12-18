fd1473a06fe568c56f57a1fea530ebf287b3092b
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-18 15:42:55 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,5 @@ class Item < ApplicationRecord
   has_many :images ,dependent: :destroy,inverse_of: :item
   belongs_to :user,optional: true
   has_one :order
-  # belongs_to :category
   accepts_nested_attributes_for :images
 end
