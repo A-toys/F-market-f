3cd2f8b04969d5338c214349fae467d0162a6b02
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-20 13:28:16 +0900

    Minor corrections

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ApplicationRecord
   belongs_to :user,optional: true
   has_one :order
   accepts_nested_attributes_for :images
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :condition, presence: true
+  validates :fee_burden, presence: true
+  validates :service, presence: true
+  validates :area, presence: true
+  validates :handing_time, presence: true
 end
