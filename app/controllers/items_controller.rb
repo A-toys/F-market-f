186a27ae9ed36c220082e1fc91bb1785dfe82094
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-23 23:57:49 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item.images.build
     render layout: 'devise'
   end
 
@@ -56,7 +55,7 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name,:text,:condition,:fee_burden,:service,:prefecture_id,:handing_time,:price,:trading_status,:service,:category_id, 
-    images_attributes:(:image_url)).merge(seller_user_id:current_user.id)
+    images_attributes:[:image_url, :_destroy, :id]).merge(seller_user_id:current_user.id)
   end
 
   def image_params
